30fc1483c252e40aada156cee0ecfbf09a44dd4d
huangjeremy <jeremy.huang@belloai.com>
2018-10-20 22:55:26 +0800

    fix bug

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ matY = np.matrix(Y, copy=True)
 
 # 模型个数，即聚类的类别个数，有多少个高斯模型
 K = 4
+iters = 100  # em算法迭代次数
 
 # 计算 GMM 模型参数
-mu, cov, alpha = GMM_EM(matY, K, 100)
+mu, cov, alpha = GMM_EM(matY, K, iters)
 
 #打印mu
 print(mu)
 
 #打印协方差
-print(conv)
+print(cov)
 
 #打印每个正态分布的分值
 print(alpha)
