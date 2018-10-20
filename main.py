f5d9ea709438f54a549e2901a566640fdf47d812
huangjeremy <jeremy.huang@belloai.com>
2018-10-20 22:52:56 +0800

    使用EM算法求出单变量的高斯混合模型参数

---
+++ main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from gmm import *
+import numpy as np
+
+# 载入数据
+with open('gmm.data') as fr:
+    Y = fr.readlines()
+Y = [[float(i.strip())] for i in Y]
+Y = np.array(Y)
+matY = np.matrix(Y, copy=True)
+
+# 模型个数，即聚类的类别个数，有多少个高斯模型
+K = 4
+
+# 计算 GMM 模型参数
+mu, cov, alpha = GMM_EM(matY, K, 100)
+
+#打印mu
+print(mu)
+
+#打印协方差
+print(conv)
+
+#打印每个正态分布的分值
+print(alpha)
